0bbeda61a5a7664a0d4a8dc3bebd2a2700b48ac8
SoleeL <carlosnr3005@gmail.com>
2020-07-28 13:50:35 -0400

    2.- respaldo

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Start the server
+const server = app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
+
+// websocket
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New connection', socket.id)
+    socket.on('chat_message', (data) => {
+        //console.log(data);
+        io.sockets.emit('chat:message', data);
+    })
+});
