a5c7437ddb79259b03d0fb0948fb526e9a2882ba
SoleeL <carlosnr3005@gmail.com>
2020-07-30 23:32:25 -0400

    Lista de conectados

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const users = [];
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -20,8 +21,25 @@ const io = SocketIO(server);
 
 io.on('connection', (socket) => {
     console.log('New connection', socket.id)
+    console.log(users)
+    socket.on('new_user', (data, callback) => {
+        if(users.includes(data)){
+            callback(false);
+        } else {
+            callback(true);
+            socket.user = data;
+            users.push(socket.user);
+            io.sockets.emit('usernames', users);
+        };
+    });
     socket.on('chat_message', (data) => {
         console.log(data);
         io.sockets.emit('chat:message', data);
-    })
+    });
+
+    socket.on('disconnect', (data) => {
+        if(!socket.user) return;
+        users.splice(users.indexOf(socket.user), 1);
+        io.sockets.emit('usernames', users);
+    });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,16 +1,18 @@
 const socket = io();
 
-
-
 // DOM elements
 let message = document.getElementById('message');
 let username = document.getElementById('username');
+let userform = document.getElementById('userform')
+let usererror = document.getElementById('usererror')
 let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
+let userslist = document.getElementById('users-list');
 
 var modal = document.getElementById("nes-modal");
 var press = document.getElementById("start");
+
 
 let logged_user;
 
@@ -19,15 +21,23 @@ window.onload = function() {
 };
 
 press.addEventListener('click', function () {
-    if (username.value !== '') {
-        logged_user= username.value;
-        modal.style.display = "none";
+    if (username.value !== '' ) {
+        console.log(username.value);
+        socket.emit('new_user', username.value, function(data){
+            console.log(data);
+            if (data) {
+                logged_user= username.value;
+                modal.style.display = "none";
+            } else {
+                usererror.innerHTML = `<p class = "nes-text is-error"> That username is already taken </p>`;
+            };
+        });
     };
 });
 
 username.addEventListener('keyup', (e) => {
-    console.log('Enter');
-    if (e.keyCode === 13 && username.value !== '') {
+    if (e.keyCode === 13) {
+        console.log('Enter');
         press.click();
     }
 }, false);
@@ -51,12 +61,26 @@ message.addEventListener('keyup', (e) => {
 socket.on('chat:message', function(data){
     output.insertAdjacentHTML("afterBegin",
     `
-    <section class="message -left">
-        <div class="nes-balloon from-left nes-pointer">
+    <div class="message -right" style="float:right">
+        <div class="nes-balloon from-right nes-pointer">
             <strong>${data.username}</strong>: ${data.message}
         </div>
-    </section>`);
+    </div>`);
     message.value = "";
+});
+
+socket.on('usernames', function(data){
+    var html = '';
+    for (user in data) {
+        html += `
+        <li>
+            <label>
+                <input type="radio" class="nes-radio is-white" name="answer-dark" />
+                <span>${data[user]}</span>  
+            </label>
+        </li>`
+    };
+    userslist.innerHTML = html;
 });
 
 
